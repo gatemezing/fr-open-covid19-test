335ec976c3109736a3502362b51a872adc8316f2
Ghislain <ghislain.atemezing@gmail.com>
2020-05-12 13:05:09 +0200

    fr home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,42 +2,30 @@
   <div class="home">
     <div class="margin-container"></div>
     <div>
-      <h1>Bienvenid@ a Open COVID-19 Test</h1>
+      <h1>Auto Evaluation Test COVID-19 selon le modèle Madrilain</h1>
       <p class="home-p">
-        Un test de autoevaluación para el COVID-19 de código abierto que
-        devuelve los mismos resultados que
-        <a href="https://www.coronamadrid.com/">coronamadrid.com</a> pero no
-        almacena tus valiosos datos.
+        Un test de auto diagnostique orientatif pour le COVID-19 en open source en FR similaire au 
+        <a href="https://www.coronamadrid.com/">coronamadrid.com</a>
+        sans conserver des données sensibles.
       </p>
+       
       <p class="home-p">
-        Si quieres más información puedes leer
-        <a
-          href="https://maldita.es/malditatecnologia/2020/03/20/aplicacion-madrid-coronavirus-oficial-comparte-datos-empresas/"
-          >este artículo de Maldita.es</a
-        >
-        donde explican que la aplicación de la Comunidad de Madrid da acceso a
-        tus datos a las empresas privadas que la han desarrollado sin
-        anonimizarlos.
-      </p>
-      <p class="home-p">
-        También puedes entrar a
+        L'algorithme original en Castellan ici
         <a href="https://github.com/celiavelmar/open-covid19-test"
-          >mi repositorio de GitHub
+          >sur GiTHub
           <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon
         ></a>
-        si quieres conocer el algoritmo que usa dicha aplicación (y también
-        esta) y cómo lo he descubierto o ver el código fuente.
+        avec le code source dont j'en ai juste fait le fork.
       </p>
       <info-panel
         class="home-info-panel"
-        text="La información obtenida a través del uso de esta aplicación no sustituye
-        ni pretende sustituir, en ningún caso, el consejo de un profesional
-        médico. Si crees que tienes síntomas compatibles con el COVID-19 ponte
-        en contacto con el teléfono correspondiente a tu Comunidad."
+        text="L'information obtenue via ce questionnaire ne remplace pas ni pretend substituer en aucun cas, 
+		l'avis d'un medecin. Si tu penses avoir des symptômes compatibles avec le COVID-19, 
+		entre en contact avec le numero vert de ta ville ou ton pays. "
       ></info-panel>
       <div class="center">
         <router-link to="/test">
-          <button>¡Entendido! Llévame al test</button>
+          <button>J'ai compris! Je fais le test</button>
         </router-link>
       </div>
     </div>
